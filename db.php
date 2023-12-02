c72c28379c726e114cfaf9d38852353b3aa53aa3
diaszin <84644190+diaszin@users.noreply.github.com>
2023-12-01 23:17:00 -0300

    Cria conexão com o banco de dados

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+define("USER", "root");
+define("PASSWORD", "");
+
+$dsn = new PDO("mysql:host=localhost;dbname=sistema-gerenciamento-de-saloes-de-festa;port=3306", USER, PASSWORD);
+
+
+
+?>

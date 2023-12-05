939c39187bc58851f7f649b31d1ad8f2e9b3d28d
diaszin <84644190+diaszin@users.noreply.github.com>
2023-12-04 23:53:37 -0300

    Add função de cadastro

--- User.php
+++ User.php
@@ -9,9 +9,18 @@ class User{
         $this->conexao = $dsn;
     }
     function login(string $email, string $senha){
+
+        $senha = password_hash($senha, 12);
         $sql = "SELECT email, senha FROM user WHERE email = ? AND senha = ?";
         $stmt = $this->conexao->prepare($sql);
         return $stmt->execute([$email, $senha]);
+    }
     
+    function signup (string $email, string $senha){
+        $senha = password_hash($senha, 12);
+        $sql = "INSERT INTO account (email, senha) VALUES (?, ?)";
+        $stmt = $this->conexao->prepare($sql);
+        
+        return $stmt->execute([$email, $senha]);
     }
 }

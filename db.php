6b255a435b47b2f56f7c572974aaab0f09936dcb
diaszin <84644190+diaszin@users.noreply.github.com>
2023-12-05 00:52:01 -0300

    Altera nome do banco de dados

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 define("USER", "root");
 define("PASSWORD", "");
 
-$dsn = new PDO("mysql:host=localhost;dbname=sistema-gerenciamento-de-saloes-de-festa;port=3306", USER, PASSWORD);
+$dsn = new PDO("mysql:host=localhost;dbname=gerenciamento_salao;port=3306", USER, PASSWORD);
 
 
 

fa7f3211e1c403b0280adac04e4fcdabdc5b769c
diaszin <84644190+diaszin@users.noreply.github.com>
2023-12-05 00:52:15 -0300

    Add validação de login

--- login.php
+++ login.php
@@ -1,22 +1,19 @@
 <?php
+include "./User.php";
 // Verifica se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // Obtém os valores do formulário
     $email = $_POST["email"];
     $senha = $_POST["senha"];
-
-    // Simples verificação de credenciais (substitua com sua lógica de autenticação real)
-    $credenciais_validas = false;
+    $user = new User();
 
     // Verifique as credenciais (exemplo: substitua isso com sua lógica real)
-    if ($email == "usuario@dominio.com" && $senha == "senha123") {
-        $credenciais_validas = true;
-    }
+    $credenciais_validas = $user->login($email, $senha);
 
     // Se as credenciais são válidas, redirecione ou exiba uma mensagem de sucesso
     if ($credenciais_validas) {
-        header("location: pagina_de_boas_vindas.php");
+        header("location: tela_opcao.html");
         exit();
     } else {
         $mensagem_erro = "Credenciais inválidas. Tente novamente.";
